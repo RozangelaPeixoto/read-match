9a2f014386f12c52590a6fc9b657fedba5ec750d
Rozangela Peixoto <roxpmelo@gmail.com>
2025-09-01 10:10:12 -0300

    add Book and Genre repository interfaces for data access

---
+++ src/main/java/com/rozangelapm/readmatch/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.rozangelapm.readmatch.repository;
+
+import com.rozangelapm.readmatch.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, String>{
+}
