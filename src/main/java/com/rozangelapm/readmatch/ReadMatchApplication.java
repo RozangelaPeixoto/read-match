fde586d5599ef866e5f06ffcb256aea9d56b07fd
Rozangela Peixoto <roxpmelo@gmail.com>
2025-09-01 10:08:52 -0300

    initialize Spring Boot application with Maven setup and configuration files

---
+++ src/main/java/com/rozangelapm/readmatch/ReadMatchApplication.java
@@ -0,0 +1,13 @@
+package com.rozangelapm.readmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadMatchApplication.class, args);
+	}
+
+}
