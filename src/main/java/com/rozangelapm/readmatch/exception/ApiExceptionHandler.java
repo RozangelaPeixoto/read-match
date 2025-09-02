f8e01b7abcb14af177799c5c8a31ecda556f6c0d
Rozangela Peixoto <roxpmelo@gmail.com>
2025-09-02 19:49:26 -0300

    refactor ApiExceptionHandler to streamline error handling; add new exception handlers

--- src/main/java/com/rozangelapm/readmatch/exception/ApiExceptionHandler.java
+++ src/main/java/com/rozangelapm/readmatch/exception/ApiExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.rozangelapm.readmatch.exception;
 
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -41,44 +43,50 @@ public class ApiExceptionHandler {
 
     @ExceptionHandler(DuplicateBookException.class)
     public ResponseEntity<Map<String, Object>> handleDuplicateBook(
-            DuplicateBookException ex, HttpServletRequest request ) {
-
-        Map<String, Object> body = new HashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.CONFLICT.value());
-        body.put("error", "Conflict");
-        body.put("message", ex.getMessage());
-        body.put("path", request.getRequestURI());
-
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(body);
+            DuplicateBookException ex, HttpServletRequest request) {
+        return buildErrorResponse(ex, request, "Conflito de recursos", true, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(HttpClientErrorException.class)
     public ResponseEntity<Map<String, Object>> handleBookNotFound(
-            HttpClientErrorException ex, HttpServletRequest request ) {
-
-        Map<String, Object> body = new HashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.NOT_FOUND.value());
-        body.put("error", "Not Found");
-        body.put("message", "Nenhum livro encontrado");
-        body.put("path", request.getRequestURI());
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
+            HttpClientErrorException ex, HttpServletRequest request) {
+        return buildErrorResponse(ex, request, "Nenhum livro encontrado", false, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(HttpServerErrorException.class)
-    public ResponseEntity<Map<String, Object>> handleBookNotFound(
-            HttpServerErrorException ex, HttpServletRequest request ) {
+    public ResponseEntity<Map<String, Object>> handleBookErrorGoogleApi(
+            HttpServerErrorException ex, HttpServletRequest request) {
+        return buildErrorResponse(ex, request, "Id inválido ou API do Google indisponível", false, HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleNotFoundEntity(
+            EntityNotFoundException ex, HttpServletRequest request) {
+        return buildErrorResponse(ex, request, "Recurso não encontrado", true, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, Object>> handleIllegalArgument(
+            IllegalArgumentException ex, HttpServletRequest request) {
+        return buildErrorResponse(ex, request, "Parâmetro inválido", true, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DateTimeParseException.class)
+    public ResponseEntity<Map<String, Object>> handleDateTimeParse(
+            DateTimeParseException ex, HttpServletRequest request) {
+        return buildErrorResponse(ex, request, "Formato de data inválido, use: aaaa-mm-dd", false, HttpStatus.BAD_REQUEST);
+    }
+
+    private ResponseEntity<Map<String, Object>> buildErrorResponse(
+            Exception ex, HttpServletRequest request, String customMessage, boolean sendExtraMessage, HttpStatus status) {
 
         Map<String, Object> body = new HashMap<>();
         body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.SERVICE_UNAVAILABLE.value());
-        body.put("error", "Service Unavailable");
-        body.put("message", "Id inválido ou API do Google indisponível");
+        body.put("status", status.value());
+        body.put("error", status.getReasonPhrase());
+        body.put("message", sendExtraMessage ? customMessage + " - " + ex.getMessage() : customMessage);
         body.put("path", request.getRequestURI());
 
-        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(body);
+        return ResponseEntity.status(status).body(body);
     }
-
 }
