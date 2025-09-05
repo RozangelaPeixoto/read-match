3eb680c5ef9733abb6bd56b08d6ab840bcc9879e
Rozangela Peixoto <roxpmelo@gmail.com>
2025-09-05 14:53:00 -0300

    enhance ReadingService to adjust preferences on rating changes; implement methods for preference updates based on book ratings

--- src/main/java/com/rozangelapm/readmatch/service/ReadingService.java
+++ src/main/java/com/rozangelapm/readmatch/service/ReadingService.java
@@ -3,9 +3,9 @@ package com.rozangelapm.readmatch.service;
 import com.rozangelapm.readmatch.dto.BookWithReadingResponse;
 import com.rozangelapm.readmatch.dto.UpdateReadingResquest;
 import com.rozangelapm.readmatch.mapper.ReadingMapper;
-import com.rozangelapm.readmatch.model.Book;
-import com.rozangelapm.readmatch.model.Reading;
-import com.rozangelapm.readmatch.model.ReadingStatus;
+import com.rozangelapm.readmatch.model.*;
+import com.rozangelapm.readmatch.repository.BookRepository;
+import com.rozangelapm.readmatch.repository.PreferenceRepository;
 import com.rozangelapm.readmatch.repository.ReadingRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
@@ -18,9 +18,13 @@ import java.util.List;
 public class ReadingService {
 
     private final ReadingRepository readingRepository;
+    private final BookRepository bookRepository;
+    private final PreferenceRepository preferenceRepository;
 
-    public ReadingService(ReadingRepository readingRepository) {
+    public ReadingService(ReadingRepository readingRepository, BookRepository bookRepository, PreferenceRepository preferenceRepository) {
         this.readingRepository = readingRepository;
+        this.bookRepository = bookRepository;
+        this.preferenceRepository = preferenceRepository;
     }
 
     public Reading createReading(Book book){
@@ -33,10 +37,10 @@ public class ReadingService {
     }
 
     @Transactional
-    public void updateReading(String id, UpdateReadingResquest updateReading){
+    public void updateReading(String bookId, UpdateReadingResquest updateReading){
 
-        Reading reading = readingRepository.findByBook_Id(id)
-                .orElseThrow(() -> new EntityNotFoundException("Leitura do livro com id: " + id));
+        Reading reading = readingRepository.findByBook_Id(bookId)
+                .orElseThrow(() -> new EntityNotFoundException("Leitura do livro com id: " + bookId));
 
         if (updateReading.status() != null) {
             reading.setStatus(ReadingStatus.fromLabel(updateReading.status()));
@@ -48,10 +52,73 @@ public class ReadingService {
             reading.setEndDate(LocalDate.parse(updateReading.endDate()));
         }
         if (updateReading.rating() != null) {
-            if(updateReading.rating() < 0 || updateReading.rating() > 5){
-                throw new IllegalArgumentException("A nota deve ser entre 0 e 5");
+            if(reading.getRating() != null){
+                adjustPreferencesForRatingChange(bookId, reading.getRating(), updateReading.rating());
+            }else{
+                if (updateReading.rating() >= 4.0) {
+                    adjustPreferencesForRatingChange(bookId, 0.0, updateReading.rating());
+                }//updatePreferencesOnRating(bookId, updateReading.rating());
             }
             reading.setRating(updateReading.rating());
+        }
+    }
+
+    @Transactional
+    private void adjustPreferencesForRatingChange(String bookId, Double oldRating, Double newRating) {
+        List<Genre> genres = bookRepository.findGenresByBookId(bookId);
+
+        for (Genre genre : genres) {
+            Preference pref = preferenceRepository.findByGenre(genre).orElseGet(() -> {
+                Preference p = new Preference(null, genre, 0.0, 0, 0.0);
+                return preferenceRepository.save(p);
+            });
+
+            double sum = pref.getRatingSum();
+            int count = pref.getRatingCount();
+
+            // caso 1: nota antiga < 4 e nova >= 4 (entrou no filtro)
+            if (oldRating < 4.0 && newRating >= 4.0) {
+                sum += newRating;
+                count += 1;
+            }
+            // caso 2: nota antiga >= 4 e nova < 4 (saiu do filtro)
+            else if (oldRating >= 4.0 && newRating < 4.0) {
+                sum -= oldRating;
+                count -= 1;
+            }
+            // caso 3: ambas >= 4 (só atualizar soma)
+            else if (oldRating >= 4.0 && newRating >= 4.0) {
+                sum = sum - oldRating + newRating;
+            }
+
+            pref.setRatingSum(sum);
+            pref.setRatingCount(count);
+            pref.setAvgRating(count > 0 ? sum / count : 0.0);
+            preferenceRepository.save(pref);
+        }
+
+    }
+
+    @Transactional
+    private void updatePreferencesOnRating(String bookId, double rating) {
+        if (rating < 4.0) {return;}
+
+        List<Genre> genres = bookRepository.findGenresByBookId(bookId);
+
+        for (Genre genre : genres) {
+            Preference pref = preferenceRepository.findByGenre(genre).orElseGet(() -> {
+                Preference p = new Preference(null, genre, 0.0, 0, 0.0);
+                return preferenceRepository.save(p);
+            });
+
+            double sum = pref.getRatingSum() + rating;
+            int count = pref.getRatingCount() + 1;
+
+            pref.setRatingSum(sum);
+            pref.setRatingCount(count);
+            pref.setAvgRating(sum / count);
+
+            preferenceRepository.save(pref);
         }
     }
 
